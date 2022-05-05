f10639ccaf696258c3c201fb363ce46047f3daae
Wanda Wang <chuwang@wustl.edu>
2022-05-05 10:31:01 -0700

    Add PetType objects in bootstrap

--- pet-clinic/pet-clinic-web/src/main/java/wanda/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/wanda/springframework/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import wanda.springframework.petclinic.model.Owner;
+import wanda.springframework.petclinic.model.PetType;
 import wanda.springframework.petclinic.model.Vet;
 import wanda.springframework.petclinic.services.OwnerService;
+import wanda.springframework.petclinic.services.PetTypeService;
 import wanda.springframework.petclinic.services.VetService;
 
 @Component
@@ -13,15 +15,27 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
   @Autowired
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    cat.setName("Cat");
+    petTypeService.save(cat);
+
+    System.out.println("Loaded PetTypes.....");
+
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
     owner1.setLastName("Weston");
