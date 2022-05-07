86821bc7edca0e8808380cbc35f4b7c2e7cf5951
Wanda Wang <chuwang@wustl.edu>
2022-05-07 11:08:41 -0700

    Implement base entity with JPA annotations

--- pet-clinic/pet-clinic-data/src/main/java/wanda/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/wanda/springframework/petclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package wanda.springframework.petclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id; // could be null
 
   public Long getId() {
