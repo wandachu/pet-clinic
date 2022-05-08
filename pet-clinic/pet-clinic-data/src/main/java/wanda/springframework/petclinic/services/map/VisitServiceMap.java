02f94548e680795e1e3721d22bbff5c857cdc8d9
Wanda Wang <chuwang@wustl.edu>
2022-05-07 22:32:30 -0700

    Add VisitServiceMap

---
+++ pet-clinic/pet-clinic-data/src/main/java/wanda/springframework/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,17 @@
+package wanda.springframework.petclinic.services.map;
+
+import wanda.springframework.petclinic.model.Visit;
+import wanda.springframework.petclinic.services.VisitService;
+
+public class VisitServiceMap extends AbstractMapService<Visit> implements VisitService {
+
+  @Override
+  public Visit save(Visit object) {
+    if (object == null || object.getPet() == null || object.getPet().getId() == null
+        || object.getPet().getOwner() == null || object.getPet().getOwner().getId() == null
+        || object.getPet().getPetType() == null || object.getPet().getPetType().getId() == null) {
+      throw new RuntimeException("Invalid visit!");
+    }
+    return super.save(object);
+  }
+}
