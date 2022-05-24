8d8e78db691d1dfd4e6f88e123b8992295fa28cb
Wanda Wang <chuwang@wustl.edu>
2022-05-24 16:32:46 -0700

    Refactor Vet

--- pet-clinic/pet-clinic-data/src/main/java/wanda/springframework/petclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/wanda/springframework/petclinic/model/Vet.java
@@ -18,12 +18,18 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Table(name = "vets")
 public class Vet extends Person {
 
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
   private Set<Specialty> specialities = new HashSet<>();
+
+  @Builder
+  public Vet(Long id, String firstName, String lastName, Set<Specialty> specialities) {
+    super(id, firstName, lastName);
+    if (specialities != null) {
+      this.specialities = specialities;
+    }
+  }
 }
